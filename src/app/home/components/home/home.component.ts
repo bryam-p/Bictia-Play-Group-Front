2d23ea4c0cf8d45614b654891e10788c4b7bed4b
Redvers Escobar Vaquer <redversescobar@gmail.com>
2020-04-25 23:31:01 -0500

    Cambios en la visualización de los videos

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Film } from 'src/app/interface/film.interface';
 import { FilmsService } from 'src/app/core/service/films/films.service';
-import Swal from "sweetalert2";
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -11,9 +11,39 @@ import Swal from "sweetalert2";
 })
 export class HomeComponent implements OnInit {
 
-  public films: Film[];
+  public videoDefault = null;
 
-  constructor(private service: FilmsService) { }
+  cerrarVideo() {
+    // Función para cerrar el reproductor del video
+    const reproductor = document.getElementById('reproductor');
+    const videoYoutube = document.getElementById('videoYoutube');
+    reproductor.classList.toggle('mostrarVideo');
+    videoYoutube.setAttribute('src', this.videoDefault);
+    console.log('Se cerró el video');
+  }
+
+  abrirVideo(filmUrl) {
+    // Función para cerrar el reproductor del video
+    const reproductor = document.getElementById('reproductor');
+    const videoYoutube = document.getElementById('videoYoutube');
+    videoYoutube.setAttribute('src', filmUrl);
+    this.videoDefault = filmUrl;
+    reproductor.classList.toggle('mostrarVideo');
+    console.log("Este es el video: ->",this.videoDefault);
+  }  
+
+  agregarFavorito(filmUrl){
+    console.log('%cConectar con la función', 'color: yellow');
+    alert("Falta conectar con la función");
+  }
+
+
+  public films: Film[];
+  public recorrido: number;
+
+  constructor(private service: FilmsService) { 
+    this.recorrido =1;
+  }
 
   ngOnInit(): void {
     this.getFilms();
@@ -36,9 +66,12 @@ export class HomeComponent implements OnInit {
           break;
         default:
           console.log('Error de conexión');
+          this.films = res.films;
+          console.log(res.films.url);
           break;
       }
     });
   }
+
 
 }
