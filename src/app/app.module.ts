0bf0ec273d861bbc2ad4f980b844270e939f3cdd
Néstor Guzmán <ing.nestor.guzman@gmail.com>
2020-04-28 00:40:33 -0500

    vista admin perfil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { SharedModule } from './shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
