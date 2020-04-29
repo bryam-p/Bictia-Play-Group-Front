f9bd2f6088c371ed4374c66a050cbd465753c450
Bryam P. Hernández <60350664+bryam-p@users.noreply.github.com>
2020-04-29 17:21:30 -0500

    Seccion play completa

--- src/app/home/components/play/play.component.ts
+++ src/app/home/components/play/play.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FilmsService } from 'src/app/core/service/films/films.service';
+import { Film } from 'src/app/interface/film.interface';
 
 @Component({
   selector: 'app-play',
@@ -7,9 +9,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayComponent implements OnInit {
 
-  constructor() { }
+  resultsSearch: any[] = []
+  public videoDefault = null;
+
+
+  cerrarVideo() {
+    // Función para cerrar el reproductor del video
+    const reproductor = document.getElementById('reproductor');
+    const videoYoutube = document.getElementById('videoYoutube');
+    reproductor.classList.toggle('mostrarVideo');
+    videoYoutube.setAttribute('src', this.videoDefault);
+    console.log('Se cerró el video');
+  }
+
+  abrirVideo(filmUrl) {
+    // Función para cerrar el reproductor del video
+    const reproductor = document.getElementById('reproductor');
+    const videoYoutube = document.getElementById('videoYoutube');
+    videoYoutube.setAttribute('src', filmUrl);
+    this.videoDefault = filmUrl;
+    reproductor.classList.toggle('mostrarVideo');
+    console.log("Este es el video: ->", this.videoDefault);
+  }
+
+  agregarFavorito(filmUrl) {
+    console.log('%cConectar con la función', 'color: yellow');
+    alert("Falta conectar con la función");
+  }
+
+
+  public films: Film[];
+  public recorrido: number;
+
+  constructor(private service: FilmsService) {
+    this.recorrido = 1;
+    this.getSearch()
+  }
 
   ngOnInit(): void {
+    this.getFilms();
   }
+
+  getSearch() {
+    this.service.$ConecctionSearch.subscribe((data: any) => {
+      console.log("Datos del search en home", data)
+      this.resultsSearch = data.results
+      const statusSearch = document.getElementById('resultado')
+      statusSearch.style.display = 'block'
+    })
+  }
+
+  getFilms() {
+    this.service.getFilms().subscribe((res: any) => {
+      console.log(res);
+      this.films = res.films;
+      console.log(res.films.url);
+
+      /*
+switch (res.statusCode) {
+        case 400:
+          Swal.fire({
+            icon: "error",
+            text: "No hay videos registrados",
+          });
+          break;
+        case 200:
+          console.log('Listado de videos');
+          this.films = res.films;
+          console.log(res.films.url);
+          break;
+        case 500:
+          console.log('Error de conexión');
+          break;
+      }
+      */
+
+    });
+  }
+
 
 }
